ce09d70dd4929e23a910e2c29d9ced039a7ed339
huyong <nghuyong@163.com>
2019-01-31 18:24:34 +0800

    MOD: fix content parse

--- sina/spiders/weibo_spider.py
+++ sina/spiders/weibo_spider.py
@@ -72,7 +72,7 @@ class WeiboSpider(RedisSpider):
 
                 else:
                     all_content = tweet_content_node.xpath('string(.)').replace('\u200b', '').strip()
-                    tweet_item['content'] = all_content
+                    tweet_item['content'] = all_content[1:]
                     yield tweet_item
 
                 yield Request(url="https://weibo.cn/{}/info".format(tweet_item['user_id']),
@@ -87,7 +87,7 @@ class WeiboSpider(RedisSpider):
         tweet_item = response.meta['item']
         content_node = tree_node.xpath('//div[@id="M_"]//span[@class="ctt"]')[0]
         all_content = content_node.xpath('string(.)').replace('\u200b', '').strip()
-        tweet_item['content'] = all_content
+        tweet_item['content'] = all_content[1:]
         yield tweet_item
 
     # 默认初始解析函数
